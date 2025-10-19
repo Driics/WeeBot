b0b98b848181c3d8e230e02a5623a8224379fb78
Nikita Kalmychin <nikitakalmycin@gmail.com>
2025-10-19 16:08:05 +0300

    Small improvements

--- sb-common-worker/src/main/kotlin/ru/sablebot/common/worker/metrics/service/DiscordMetricsRegistry.kt
+++ sb-common-worker/src/main/kotlin/ru/sablebot/common/worker/metrics/service/DiscordMetricsRegistry.kt
@@ -1,15 +1,15 @@
 package ru.sablebot.common.worker.metrics.service
 
 import io.micrometer.core.instrument.Gauge
-import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.binder.MeterBinder
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 import ru.sablebot.common.worker.shared.service.DiscordService
 
 @Component
-class DiscordMetricsRegistry {
+class DiscordMetricsRegistry(
+    private val discordService: DiscordService
+) {
     companion object {
         const val GAUGE_GUILDS: String = "discord.guilds"
         const val GAUGE_USERS: String = "discord.users"
@@ -19,69 +19,63 @@ class DiscordMetricsRegistry {
         const val GAUGE_PING: String = "discord.average.ping"
     }
 
-    @Autowired
-    private lateinit var discordService: DiscordService
-
-    @Autowired
-    private lateinit var meterRegistry: MeterRegistry
-
     private val shardManager
         get() = discordService.shardManager
 
     @Bean
-    fun gaugeGuilds() = MeterBinder {
+    fun gaugeGuilds() = MeterBinder { registry ->
         Gauge.builder(GAUGE_GUILDS, shardManager) {
             runCatching { it.guildCache.size().toDouble() }.getOrElse { 0.0 }
         }
             .description("Number of Discord guilds")
-            .register(meterRegistry)
-    }.bindTo(meterRegistry)
+            .register(registry)
+    }
 
     @Bean
-    fun gaugeUsers() = MeterBinder {
+    fun gaugeUsers() = MeterBinder { registry ->
         Gauge.builder(GAUGE_USERS, shardManager) {
             runCatching { it.userCache.size().toDouble() }.getOrElse { 0.0 }
         }
             .description("Number of Discord users")
-            .register(meterRegistry)
-    }.bindTo(meterRegistry)
+            .register(registry)
+    }
 
     @Bean
-    fun gaugePing() = MeterBinder {
+    fun gaugePing() = MeterBinder { registry ->
         Gauge.builder(GAUGE_PING, shardManager) {
             runCatching { it.averageGatewayPing }.getOrElse { 0.0 }
         }
             .description("Average gateway ping in milliseconds")
             .baseUnit("ms")
-            .register(meterRegistry)
-    }.bindTo(meterRegistry)
+            .register(registry)
+    }
 
     @Bean
-    fun gaugeVoiceChannelCount() = MeterBinder {
+    fun gaugeVoiceChannelCount() = MeterBinder { registry ->
         Gauge.builder(GAUGE_VOICE_CHANNELS, shardManager) {
             runCatching { it.voiceChannelCache.size().toDouble() }.getOrElse { 0.0 }
         }
             .description("Number of voice channels")
-            .register(meterRegistry)
-    }.bindTo(meterRegistry)
+            .register(registry)
+    }
 
     @Bean
-    fun gaugeTextChannelCount() = MeterBinder {
+    fun gaugeTextChannelCount() = MeterBinder { registry ->
         Gauge.builder(GAUGE_TEXT_CHANNELS, shardManager) {
             runCatching { it.textChannelCache.size().toDouble() }.getOrElse { 0.0 }
         }
             .description("Number of text channels")
-            .register(meterRegistry)
-    }.bindTo(meterRegistry)
+            .register(registry)
+    }
 
     @Bean
-    fun gaugeChannelCount() = MeterBinder {
+    fun gaugeChannelCount() = MeterBinder { registry ->
         Gauge.builder(GAUGE_CHANNELS, shardManager) {
             runCatching {
                 (it.voiceChannelCache.size() + it.textChannelCache.size()).toDouble()
             }.getOrElse { 0.0 }
         }
             .description("Total number of channels (voice + text)")
-            .register(meterRegistry)
-    }.bindTo(meterRegistry)
+            .register(registry)
+    }
 }
