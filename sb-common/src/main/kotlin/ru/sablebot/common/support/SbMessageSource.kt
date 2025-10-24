86d9c4dc02a33c5f577ab6ec8bbf48459c8af2ad
Nikita Kalmychin <nikitakalmycin@gmail.com>
2025-10-24 22:07:36 +0300

    Trying to add i8n

--- sb-common/src/main/kotlin/ru/sablebot/common/support/SbMessageSource.kt
+++ sb-common/src/main/kotlin/ru/sablebot/common/support/SbMessageSource.kt
@@ -6,28 +6,12 @@ import java.text.MessageFormat
 import java.util.*
 
 @Component
-class SbMessageSource(
+open class SbMessageSource(
     private val messageSources: List<ModuleMessageSource>
 ) : AbstractMessageSource() {
-    override fun resolveCodeWithoutArguments(code: String, locale: Locale): String? {
-        messageSources.forEach { messageSource ->
-            val result = messageSource.resolveCodeWithoutArguments(code, locale)
-            if (result != null) {
-                return result
-            }
-        }
+    override fun resolveCodeWithoutArguments(code: String, locale: Locale): String? =
+        messageSources.firstNotNullOfOrNull { it.resolveCodeWithoutArguments(code, locale) }
 
-        return null
-    }
-
-    override fun resolveCode(code: String, locale: Locale): MessageFormat? {
-        messageSources.forEach { messageSource ->
-            val result = messageSource.resolveCode(code, locale)
-            if (result != null) {
-                return result
-            }
-        }
-
-        return null
-    }
+    override fun resolveCode(code: String, locale: Locale): MessageFormat? =
+        messageSources.firstNotNullOfOrNull { it.resolveCode(code, locale) }
 }
