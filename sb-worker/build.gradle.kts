5d8189b07dfc8ae8f96ba68c955d50d105638b8b
Nikita Kalmychin <nikitakalmycin@gmail.com>
2026-03-07 14:19:30 +0300

    build: convert sb-worker to KTS with version catalog

---
+++ sb-worker/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("sablebot.spring-boot-app")
+}
+
+description = "SableBot Discord Bot Worker Application"
+
+application {
+    mainClass.set("ru.sablebot.worker.Launcher")
+}
+
+tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    archiveFileName.set("SableBot-Worker.jar")
+}
+
+dependencies {
+    implementation(libs.spring.boot.starter.web)
+
+    implementation(project(":sb-common-worker"))
+    implementation(project(":modules:sb-module-moderation"))
+    implementation(project(":modules:sb-module-audio"))
+
+    implementation(libs.groovy)
+}
